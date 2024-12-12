efae4d189984752a5b3a17ea091e265b33a3e4f4
yeonsu <bestdustn@sookmyung.ac.kr>
2024-12-12 12:08:45 +0900

    [TEST] Add idempotency test for Dollar.times() method - Dollar.times() 메서드의 멱등성 테스트 추가

--- src/test/java/com/example/tddexample/TestClass.java
+++ src/test/java/com/example/tddexample/TestClass.java
@@ -13,10 +13,10 @@ public class TestClass {
         Dollar result2 = dollar.times(2);
 
         // 첫 호출과 두 번째 호출의 결과가 동일한지 확인
-        assertEquals(result1.amount, result2.amount);
+        assertEquals(result1.getAmount(), result2.getAmount());
 
         // 원래 객체가 변경되지 않았는지 확인 (불변 객체 여부 확인)
-        assertEquals(5, dollar.amount);
+        assertEquals(5, dollar.getAmount());
     }
 }
 
