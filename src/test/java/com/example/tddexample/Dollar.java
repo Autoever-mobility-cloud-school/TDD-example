ae72face60032fb104ccf48bbe4c2d4654b08279
yeonsu <bestdustn@sookmyung.ac.kr>
2024-12-12 11:14:57 +0900

    [FIX] Update Dollar.java - 상수를 모두 변수로 치환

--- src/test/java/com/example/tddexample/Dollar.java
+++ src/test/java/com/example/tddexample/Dollar.java
@@ -3,11 +3,10 @@ package com.example.tddexample;
 public class Dollar {
     int amount;
     public Dollar(int amount){
-
+        this.amount = amount;
     }
 
     public void times(int multiplier){
-
+        amount = amount * multiplier;
     }
 }
-
